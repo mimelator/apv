fdd0b521a2924bf38f6276326f637b6bed4a81d1
markimel <markimel@192.168.27.11>
2018-04-03 17:19:46 -0700

    initial base class

---
+++ APV/src/com/arranger/apv/systems/lite/cycle/LiteCycleShapeSystem.java
@@ -0,0 +1,22 @@
+package com.arranger.apv.systems.lite.cycle;
+
+import com.arranger.apv.Main;
+import com.arranger.apv.systems.lite.LiteShapeSystem;
+
+public abstract class LiteCycleShapeSystem extends LiteShapeSystem {
+
+	public LiteCycleShapeSystem(Main parent) {
+		super(parent);
+	}
+
+	@Override
+	public void setup() {
+
+	}
+
+	@Override
+	public void draw() {
+
+	}
+
+}
