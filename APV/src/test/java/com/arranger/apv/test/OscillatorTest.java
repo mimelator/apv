2c9990578bf352ed891a9395549be0376ce2a5f8
markimel <markimel@192.168.27.11>
2018-04-24 06:23:10 -0700

    Fixed test

--- APV/src/test/java/com/arranger/apv/test/OscillatorTest.java
+++ APV/src/test/java/com/arranger/apv/test/OscillatorTest.java
@@ -1,12 +1,42 @@
 package com.arranger.apv.test;
 
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 import com.arranger.apv.util.Oscillator;
+import com.arranger.apv.util.SplineHelper;
 
 public class OscillatorTest extends APVPluginTest {
 
-
+	private static final float EPSILON = 0.001f;
+	
+	@BeforeEach
+	public void setup() {
+		SplineHelper sh = new SplineHelper(parent);
+		
+		when(parent.mapEx(
+				Mockito.anyFloat(), 
+				Mockito.anyFloat(), 
+				Mockito.anyFloat(), 
+				Mockito.anyFloat(), 
+				Mockito.anyFloat())).thenAnswer(new Answer<Float>() {
+					@Override
+			        public Float answer(InvocationOnMock inv) throws Throwable {
+						float value = inv.getArgument(0);
+						float start = inv.getArgument(1);
+						float end = inv.getArgument(2);
+						float start1 = inv.getArgument(3);
+						float end1 = inv.getArgument(4);
+						return sh.map(value, start, end, start1, end1);
+					}
+				});
+	}
+	
 	@Test
 	void testOscillator() throws Exception {
 		Oscillator o = new Oscillator(parent);
@@ -24,10 +54,13 @@ public class OscillatorTest extends APVPluginTest {
 			advanceFrame();
 		}
 		
-		assert(min == 0.0f);
-		assert(max == 1.0f);
+		assert(isEqual(min,  0.0f));
+		assert(max >= 1.0f); //So now that oscillate uses the Spline Helper it overshoots the max range by about 30%
 		
-		//throw new Exception("testOscillator needs to be implemented");
+	}
+	
+	protected boolean isEqual(float v1, float v2) {
+		return Math.abs(v1 - v2) < EPSILON;
 	}
 
 	@Override
