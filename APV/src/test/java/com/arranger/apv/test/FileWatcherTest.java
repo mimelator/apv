c3822fa8788540958340eda355844c9eecd1a0a9
markimel <markimel@192.168.27.11>
2018-04-25 19:09:28 -0700

    File Watcher

---
+++ APV/src/test/java/com/arranger/apv/test/FileWatcherTest.java
@@ -0,0 +1,51 @@
+package com.arranger.apv.test;
+
+import java.io.File;
+import java.util.Random;
+
+import org.junit.jupiter.api.Test;
+
+import com.arranger.apv.util.FileHelper;
+import com.arranger.apv.util.FileWatcher;
+
+public class FileWatcherTest extends APVPluginTest {
+	
+	private static final String FILE_TO_WATCH = "test.txt";
+	private boolean changed = false;
+	private int target;
+	
+	public FileWatcherTest() {
+	}
+
+	
+	@Test
+	public void testFileWatcher() throws Exception {
+		FileHelper fh = new FileHelper(parent);
+		final File file = new File(fh.getFullPath(FILE_TO_WATCH));
+		System.out.println("watching: " + file.getAbsolutePath());
+		new FileWatcher(file, () -> {
+			changed = true;
+		}).start();
+		
+		Thread.yield();
+		target = new Random().nextInt();
+		fh.saveFile(FILE_TO_WATCH, "Hello There World: " + target);
+		
+		while (!changed) {
+			Thread.sleep(1000);
+		}
+		
+		String contents = fh.readFile(FILE_TO_WATCH);
+		assert(contents != null);
+		assert(!contents.isEmpty());
+		int index = contents.indexOf(String.valueOf(target));
+		assert(index != -1);
+	}
+	
+	
+	@Override
+	protected void setFrameIndexes() {
+
+	}
+
+}
