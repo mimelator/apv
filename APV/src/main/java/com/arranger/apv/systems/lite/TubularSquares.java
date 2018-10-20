906def9b8721e641b3f0aecf8cd0a57df4fd8c3e
markimel <mimel@imelshire.com>
2018-10-20 05:20:51 -0700

    tweaking the square in Tubular Squares

--- APV/src/main/java/com/arranger/apv/systems/lite/TubularSquares.java
+++ APV/src/main/java/com/arranger/apv/systems/lite/TubularSquares.java
@@ -2,6 +2,8 @@ package com.arranger.apv.systems.lite;
 
 import com.arranger.apv.Main;
 
+import processing.core.PApplet;
+
 public class TubularSquares extends Tubular {
 
 	public TubularSquares(Main parent) {
@@ -17,5 +19,12 @@ public class TubularSquares extends Tubular {
 	@Override
 	protected void drawTubeShape(float tubeSide) {
 		parent.rect(parent.width / 2, parent.height / 2, tubeSide, tubeSide);
+	}
+	
+	@Override
+	protected void drawGridItem(int i, int j, float dist) {
+		parent.rect(i, j, 
+				PApplet.sq(PApplet.sqrt(SQUARE_SIDE - dist)), 
+				PApplet.sq(PApplet.sqrt(SQUARE_SIDE - dist)));
 	}
 }
