bb0d37bab2d92832c2e9d2cb9f60c01d47e5d459
EC2 Default User <ec2-user@ip-172-31-34-184.us-west-2.compute.internal>
2019-06-14 23:54:09 +0000

    fixing github 8

--- Release/bin/wavelength.bat
+++ Release/bin/wavelength.bat
@@ -1,6 +1,3 @@
 
-set JRE=jre1.8.0_161
-
-
-..\Plugins\%JRE%\bin\java.exe -Dconfig.file=..\conf\application.conf^
+..\Plugins\bin\java.exe -Dconfig.file=..\conf\application.conf^
  -classpath ..\Java\ com.arranger.apv.Main
